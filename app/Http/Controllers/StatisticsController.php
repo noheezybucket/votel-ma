d3ab00234c9366b1321ce9d83dcd11f12adef009
Mouhamad Gueye <seydinag023@gmail.com>
2024-01-13 05:02:43 +0000

    added colors to the charts

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -19,11 +19,11 @@ class StatisticsController extends Controller
 
         $chart1 = new CandidatesNumber;
         $chart1->labels($candidats->keys());
-        $chart1->dataset('My dataset 1', 'bar', $candidats->values())->backgroundColor(['#000', '#444']);
+        $chart1->dataset('My dataset 1', 'bar', $candidats->values())->backgroundColor(['#F42B03', '#1B2CC1', '#1EFFBC']);
 
         $chart2 = new CandidatesNumber;
         $chart2->labels($programmes->keys());
-        $chart2->dataset('My dataset 2', 'doughnut', $programmes->values())->backgroundColor(['#000', '#444']);
+        $chart2->dataset('My dataset 2', 'doughnut', $programmes->values())->backgroundColor(['#F42B03', '#1B2CC1', '#1EFFBC']);
 
 
         return view('/electeur/stats', compact('chart1', 'chart2'));
