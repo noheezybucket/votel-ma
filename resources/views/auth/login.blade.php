94e2efe86cf4c903b8dab00a467c9a0fa31da7a8
Mouhamad Gueye <seydinag023@gmail.com>
2024-01-13 05:47:42 +0000

    style auth pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,18 @@
 @extends('auth-base')
 
-
+@section('title', 'Connexion')
 @section('auth-img')
     <img src="{{ asset('img/login.jpg') }}" class="w-100 h-100 object-fit-cover" alt="Vote">
 @endsection
 
 @section('auth-form')
     <form id="login-form" class="shadow-sm border rounded p-3" style="max-width: 500px; width:100%">
-        <div class="d-flex justify-content-center align-items-center text-2xl  rounded p-2 bg-light text-primary fw-bold">
-            <x-far-envelope style="width: 50px; margin-right:5px" />
-            <span class="fs-3">ParrainApp</span>
+        <div
+            class="d-flex justify-content-center align-items-center text-2xl mb-2 rounded p-2 bg-light text-primary fw-bold">
+            <x-far-circle-user style="width: 50px; margin-right:5px" />
+
+            <span class="fs-3">Connexion</span>
         </div>
-        <h3 class="my-3 text-center fs-6">Accédez facilement à votre compte </h3>
         @csrf
         <div class=" mb-2">
             <label for="email" class="form-label">Email</label>
@@ -24,7 +25,10 @@
                 oublié?</a>
         </div>
         <div class=" mb-2">
-            <button type="button" class="btn btn-primary container-fluid" id="login-btn">Se connecter</button>
+            <button type="button" class="btn btn-outline-primary container-fluid" id="login-btn">Se connecter
+                <x-fas-arrow-right-to-bracket style="width: 20px; margin-right:5px" />
+
+            </button>
         </div>
         <div class="mb4">
             <p><span id="error" class="text-danger"></span></p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,29 +1,35 @@
 @extends('auth-base')
+@section('title', 'Insription')
 
 @section('auth-img')
     <img src="{{ asset('img/register.jpg') }}" class="w-100 h-100 object-fit-cover" alt="Vote">
 @endsection
 
 @section('auth-form')
-    <form id="login-form" class="shadow-sm border rounded p-3" style="max-width: 500px; width:100%">
-        <div class="d-flex justify-content-center align-items-center text-2xl  rounded p-2 bg-light text-primary fw-bold">
-            <x-far-envelope style="width: 50px; margin-right:5px" />
-            <span class="fs-3">ParrainApp</span>
+    <form id="login-form" class="shadow-sm border rounded p-3 " style="max-width: 500px; width:100%">
+        <div
+            class="d-flex justify-content-center align-items-center text-2xl mb-2  rounded p-2 bg-light text-primary fw-bold">
+            <x-far-circle-user style="width: 50px; margin-right:5px" />
+            <span class="fs-3">Inscription</span>
         </div>
-        <h3 class="my-3 text-center fs-6">Inscrivez facilement en remplissant le formulaire </h3>
         @csrf
         <div class=" mb-2">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" id="email" name="email" class="form-control">
+            <label for="nomcomplet" class="form-label">Nom Complet</label>
+            <input type="text" id="nomcomplet" name="nomcomplet" class="form-control">
+        </div>
+        <div class=" mb-2">
+            <label for="nci" class="form-label">N° Carte d'identité</label>
+            <input type="text" id="nci" name="nci" class="form-control">
         </div>
         <div class=" mb-2">
             <label for="password" class="form-label">Mot de passe</label>
             <input type="password" id="password" name="password" class="form-control">
-            <a href="" class="text-decoration-none d-block mt-2">Mot de passe
-                oublié?</a>
+
         </div>
-        <div class=" mb-2">
-            <button type="button" class="btn btn-primary container-fluid" id="login-btn">Se connecter</button>
+        <div class=" my-2">
+            <button type="button" class="btn btn-outline-primary container-fluid" id="register-btn">S'inscrire
+                <x-fas-arrow-right-to-bracket style="width: 20px; margin-right:5px" />
+            </button>
         </div>
         <div class="mb4">
             <p><span id="error" class="text-danger"></span></p>
@@ -36,5 +42,3 @@
         </div>
     </form>
 @endsection
-
-{{-- @include('scripts/auth') --}}
